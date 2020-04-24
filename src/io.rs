8507fe7017bd3cb05d7a41fb21952e5b5b0046c9
Duncan McGreggor <oubiwann@gmail.com>
2020-04-24 01:58:25 -0500

    Added support for per-pixel hashing.

--- src/io.rs
+++ src/io.rs
@@ -2,8 +2,10 @@ use crate::color::{self, Color};
 use image::{Rgba, RgbaImage};
 use ron;
 use serde::{Deserialize, Serialize};
+use std::collections::hash_map::DefaultHasher;
 use std::collections::HashMap;
 use std::fs::File;
+use std::hash::{Hash, Hasher};
 
 pub fn open(path: &str) -> RgbaImage {
     return image::open(path).unwrap().to_rgba();
@@ -86,6 +88,12 @@ impl Manager {
         }
     }
 
+    pub fn hash(&self, x: u32, y: u32) -> u64 {
+        let mut hasher = DefaultHasher::new();
+        self.get(x, y).hash(&mut hasher);
+        return hasher.finish();
+    }
+
     pub fn show_names(&self) {
         for pixel in self.image.pixels() {
             let c = pixel.paint("██");
@@ -103,6 +111,14 @@ pub struct PixelData {
     pub y: u32,
     pub color: Rgba<u8>,
     pub color_name: String,
+}
+
+impl Hash for PixelData {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.x.hash(state);
+        self.y.hash(state);
+        self.color.hash(state);
+    }
 }
 
 #[cfg(test)]
@@ -156,5 +172,15 @@ mod tests {
         assert_eq!(pixel_data.y, 0);
         assert_eq!(pixel_data.color.rgba(), [255, 0, 0, 255]);
         assert_eq!(pixel_data.color_name, "red");
+    }
+
+    #[test]
+    fn test_manager_hash() {
+        let opts = ManagerOptions {
+            image_path: String::from("src/test-image.png"),
+            color_file_path: String::from("src/test-color-file.ron"),
+        };
+        let manager = Manager::new(opts);
+        assert_eq!(manager.hash(0, 0), 14349191598533205778);
     }
 }
